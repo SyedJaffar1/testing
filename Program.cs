9735e02621089f8ada1cbb35fbeca3c51a6c2b04
Jaffar <sajaffar533@gmail.com>
2020-12-31 11:10:27 +0500

    committed at 1110

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testing1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
